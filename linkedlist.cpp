1b8273046bd6890853b16d5f5c67d95e9faef5ef
Shikha Sharma <shikha735@gmail.com>
2016-04-29 18:53:38 +0530

    All the methods added

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -33,6 +33,7 @@ void insertEnd(int val){
     return;
 }
 
+// insert at nth position
 void insertNth(int val, int position){
     Node *newnode = new Node();
     newnode->data = val;
@@ -51,6 +52,7 @@ void insertNth(int val, int position){
     temp->next = newnode;
 }
 
+// delete from beginning
 void deleteBeg(){
     Node *temp = new Node();
     temp = head;
@@ -59,6 +61,8 @@ void deleteBeg(){
     return;
 }
 
+
+// delete from the end
 void deleteEnd(){
     Node *pres = new Node();
     Node *prev = new Node();
@@ -72,6 +76,8 @@ void deleteEnd(){
     return;
 }
 
+
+// delete from nth position
 void deleteNth(int position){
     Node *temp = new Node();
     temp = head;
@@ -100,6 +106,45 @@ void display(){
     return;
 }
 
+// display linked list in reverse order
+void displayReverse(Node* head){
+    Node *temp = head;
+    if(temp!=NULL){
+        displayReverse(temp->next);
+        cout << temp->data << '\t';
+    }
+    return;
+}
+
+// reversing a linked list iteratively
+void reverseIterative(){
+	Node *curr, *prev, *currnext;
+	prev = NULL;
+	curr = head;
+	while(curr!=NULL){
+        currnext = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = currnext;
+	}
+	head = prev;
+}
+
+// reversing a linked list using recursion
+void reverseRecursive(Node *temp){
+    if(temp->next == NULL){
+        head = temp;
+        return;
+    }
+    reverseRecursive(temp->next);
+    // Node *prev = temp->next;
+    // prev->next = temp;
+    // temp->next = NULL;
+    temp->next->next = temp;
+    temp->next = NULL;
+}
+
+// main function
 int main(){
 
     head = NULL;
@@ -107,7 +152,8 @@ int main(){
 
     do{
         cout << "\nMenu:" << endl;
-        cout << "\n1. InsertBegin \n2. InsertEnd \n3. Insert at nth position \n4. DeleteBegin \n5. DeleteEnd \n6. Delete from nth position \n7. Display \n8. Exit \nEnter option: ";
+        cout << "\n1. InsertBegin \n2. InsertEnd \n3. Insert at nth position \n4. DeleteBegin \n5. DeleteEnd \n6. Delete from nth position \n7. Display \n8. Exit";
+        cout << " \n9. Reverse Iterative \n10. Reverse Recursive \n11. Display in reverse order \nEnter option: ";
         cin >> option;
         switch(option){
         case 1: cout << "Enter the value to insert: ";
@@ -159,6 +205,15 @@ int main(){
                 break;
 
         case 8: exit(0);
+
+        case 9: reverseIterative();
+                break;
+
+        case 10: reverseRecursive(head);
+                 break;
+
+        case 11: displayReverse(head);
+                 break;
 
         default: cout << "Enter valid option" << endl;
         }
