83d0d1639cf94b9c3baa346b2fee54fdee005da3
Shikha Sharma <shikha735@gmail.com>
2016-05-07 22:23:38 +0530

    Tree traversals without using recursion
    Iterative approach using stack

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <stack>
 #include <queue>
 
 using namespace std;
@@ -57,32 +58,91 @@ void levelOrder(BstNode *root){
 	}
 }
 
-// Preorder traversal is used to create a copy of the tree.
+// Preorder traversal is used to create a copy of the tree. Time - O(n) Space -O(h)
 // Preorder traversal is also used to get prefix expression on of an expression tree.
+void recPreorder(BstNode* root){
+    if(root!=NULL){
+        cout << root->data << '\t';
+        recPreorder(root->left);
+        recPreorder(root->right);
+    }
+}
+
+// non recursive, iterative approach using stack
 void preorder(BstNode* root){
-    if(root!=NULL){
-        cout << root->data << '\t';
-        preorder(root->left);
-        preorder(root->right);
+    if(root == NULL) return;
+    stack<BstNode*> S;
+    BstNode* current = root;
+    S.push(current);
+    while(!S.empty()){
+        current = S.top();
+        S.pop();
+        cout << current->data << '\t' ;
+        if(current->right != NULL)
+            S.push(current->right);
+        if(current->left != NULL)
+            S.push(current->left);
     }
 }
 
-// Inorder traversal gives nodes in non-decreasing order.
+// Inorder traversal gives nodes in non-decreasing order. Time - O(n) Space -O(h)
+void recInorder(BstNode* root){
+    if(root!=NULL){
+        recInorder(root->left);
+        cout << root->data << '\t';
+        recInorder(root->right);
+    }
+}
+
+// non recursive, iterative approach using stack
 void inorder(BstNode* root){
-    if(root!=NULL){
-        inorder(root->left);
-        cout << root->data << '\t';
-        inorder(root->right);
+    if(root == NULL) return;
+    stack<BstNode*> S;
+    BstNode* current = root;
+    while(1){
+        if(current!=NULL){
+            S.push(current);
+            current = current->left;
+        }
+        else{
+            if(S.empty()) break;
+            current = S.top();
+            S.pop();
+            cout << current->data << '\t';
+            current = current->right;
+        }
     }
 }
 
-//Postorder traversal is used to delete the tree
+//Postorder traversal is used to delete the tree. Time - O(n) Space -O(h)
 // and to get the postfix expression of an expression tree.
-void postorder(BstNode* root){
+void recPostorder(BstNode* root){
     if(root!=NULL){
-        postorder(root->left);
-        postorder(root->right);
+        recPostorder(root->left);
+        recPostorder(root->right);
         cout << root->data << '\t';
+    }
+}
+
+// non recursive, iterative approach using stack
+void postorder(BstNode* root){
+    if(root == NULL) return;
+    stack<BstNode*> S1, S2;
+    BstNode* current = root;
+    S1.push(current);
+    while(!S1.empty()){
+        current = S1.top();
+        S1.pop();
+        S2.push(current);
+        if(current->left != NULL)
+            S1.push(current->left);
+        if(current->right != NULL)
+            S1.push(current->right);
+    }
+    while(!S2.empty()){
+        current = S2.top();
+        S2.pop();
+        cout << current->data << '\t';
     }
 }
 
@@ -98,7 +158,8 @@ int main(){
 
     do{
         cout << "\nMenu:" << endl;
-        cout << "1. Insert \n2. Search \n3. Preorder Traversal \n4. Inorder Traversal \n5. Postorder Traversal \n6. Height \n7. Level Order \n8. Exit \nEnter option: " << endl;
+        cout << "1. Insert \n2. Search \n3. Preorder Traversal \n4. Inorder Traversal \n5. Postorder Traversal \n6. Height \n7. Level Order \n8. Exit ";
+		cout << "\n*** Without recursion *** \n9. Preorder \n10. Inorder \n11. Postorder \nEnter option: " << endl;
         cin >> option;
 
         switch(option){
@@ -116,13 +177,13 @@ int main(){
                         cout << "Value not found";
                     break;
 
-            case 3: preorder(root);
+            case 3: recPreorder(root);
                     break;
 
-            case 4: inorder(root);
+            case 4: recInorder(root);
                     break;
 
-            case 5: postorder(root);
+            case 5: recPostorder(root);
                     break;
 
             case 6: cout << "Height of the BST is: " << findHeight(root);
@@ -132,6 +193,15 @@ int main(){
                     break;
 
             case 8: exit(0);
+
+            case 9: preorder(root);
+                    break;
+
+            case 10: inorder(root);
+                     break;
+
+            case 11: postorder(root);
+                     break;
         }
     }while(1);
     return 0;
