75db4fce78d15875c3bcbe3f5574bf76650a1fba
Shikha Sharma <shikha735@gmail.com>
2016-06-09 01:08:05 +0530

    ToggleBits added

---
+++ Bit Manipulation/ToggleBits.cpp
@@ -0,0 +1,24 @@
+// C++ program to toggle bits - Bit manipulation
+#include <iostream>
+using namespace std;
+
+int toggleBits(int n){
+    if(n == 0) return 1;
+    int nextSetBit = 1, lastBit, solution = 0;
+    while(n != 0){
+        lastBit = n&1;
+        if(lastBit == 0){
+            solution |= nextSetBit;
+        }
+        nextSetBit <<= 1;
+        n = unsigned(n) >> 1;
+    }
+    return solution;
+}
+
+int main(){
+    int input;
+    cin >> input;
+    cout << toggleBits(input);
+    return 0;
+}
